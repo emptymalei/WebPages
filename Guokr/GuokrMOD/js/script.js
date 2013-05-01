0821532830f14ff96911542ffb6147532ef3380f
emptymalei <emptymalei@qq.com>
2013-05-01 20:59:47 +0800

    Add GuokrMOD Extension

---
+++ Guokr/GuokrMOD/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$("body").css("background-color","white");
+});
+
+$(document).ready(function(){
+$("#blockWindow").append("<iframe src='http://www.sciweavers.org/free-online-latex-equation-editor'></iframe>");
+});
+
+$(document).ready(function(){
+temp=$("tab1").val();
+$("body").append(temp);
+});
