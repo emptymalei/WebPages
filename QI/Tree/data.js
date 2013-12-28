2e5bbbe8b429e8aa565c472d8abb434b8e6feaf7
emptymalei <emptymalei@qq.com>
2013-12-27 19:50:00 -0700

    JqTree Test

---
+++ QI/Tree/data.js
@@ -0,0 +1,15 @@
+[
+    {
+        label: 'node1',
+        children: [
+            { label: 'child1' },
+            { label: 'child2' }
+        ]
+    },
+    {
+        label: 'node2',
+        children: [
+            { label: 'child3' }
+        ]
+    }
+]
